08706ab60fe724bb1bc2465a1e5020948d7cc86e
Greenbirch2007 <291109028@qq.com>
2020-03-12 19:53:43 +0800

    初步可以下载o

--- details.py
+++ details.py
@@ -1,13 +1,12 @@
-import requests
-import re
+
+#! -*- coding:utf-8 -*-
+import datetime
 import urllib
-import time
-from requests.exceptions import RequestException  #用于捕捉异常
-from multiprocessing import Pool
-import random
+
 
 import os
 
+from id_s import l_id
 # 通过算法优势把图片给弄下来
 #关键是找到id
 
@@ -15,14 +14,55 @@ import os
 
 
 
+import asyncio
+import aiohttp
+
+
+def run_forever(func):
+    def wrapper(obj):
+        while True:
+            func(obj)
+    return wrapper
 
 
 
 
 
-ln = os.getcwd()
-url__p = 'https://cdn.japancupid.com/memphoto/Photo4/big/2883065.jpg'
-urllib.request.urlretrieve(url__p, '{0}/{1}.jpg'.format(ln,url__p[-11:-4]+url__p[-22:-16]))
+
+async def get_title(i1,i2):
+
+
+    url__p = 'https://cdn.japancupid.com/memphoto/Photo{0}/big/{1}.jpg'.format(i1,i2)
+
+
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url__p) as resp:
+            try:
+
+                ln = os.getcwd()
+                await urllib.request.urlretrieve(url__p, '{0}/{1}.jpg'.format(ln, url__p[-11:-4] + url__p[-22:-16]))
+            except:
+                pass
+
+
+
+
+if __name__ == "__main__":
+    print(datetime.datetime.now())
+    s = datetime.datetime.now()
+
+
+    loop = asyncio.get_event_loop()
+    fun_list = (get_title(i1,i2) for i1 in range(1,4) for i2 in l_id)
+    loop.run_until_complete(asyncio.gather(*fun_list))
+    print(datetime.datetime.now())
+    e = datetime.datetime.now()
+    all_t = e-s
+
+
+
+
 
 
 
