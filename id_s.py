9049264250835ec08f26cb752a282e72b775b282
Greenbirch2007 <291109028@qq.com>
2020-03-12 10:11:42 +0800

    需要验证登录，比较繁琐暂放

--- id_s.py
+++ id_s.py
@@ -15,68 +15,9 @@ from requests.exceptions import RequestException
 
 
 
-import asyncio
-import aiohttp
-
-
-def run_forever(func):
-    def wrapper(obj):
-        while True:
-            func(obj)
-    return wrapper
 
 
 
-
-
-
-
-# async def get_title(i):
-
-#     url = 'https://www.japancupid.com/en/results/matches?pageno={0}'
-
-
-
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as resp:
-#             print(resp.status)
-#             text = await resp.text()
-#             print('start', i)
-
-#     big_list = []
-#     al_id = re.compile('<a id="(.*?)" class="absolute top-minus-100"></a>',re.S)
-#     items = re.findall(text,al_id)
-#     for item in items:
-#         big_list.append(item)
-#     print(items)
-#     time.sleep(8)
-    
-
-
-
-
-
-    # connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='Yahoo_J',charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-    # cursor = connection.cursor()
-    # try:
-    #     cursor.executemany('insert into Tokyo_CFA (salary,type,link,job_name) values (%s,%s,%s,%s)', big_list)
-    #     connection.commit()
-    #     connection.close()
-    #     print('向MySQL中添加数据成功！')
-    # except TypeError :
-    #     pass
-
-
-
-
-def call_page(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
-        return None
-    except RequestException:
-        return None
 
 
 def parse_html(html):
@@ -88,8 +29,88 @@ def parse_html(html):
 # fun_list = (get_title(i) for i in range(1,31))
 # loop.run_until_complete(asyncio.gather(*fun_list))
 
-url = 'https://www.japancupid.com/en/results/matches?pageno=3'
 
 
-html = call_page(url)
-print(html)
+
+
+
+
+
+
+# -*- coding:utf-8 -*-
+import datetime
+import re
+import time
+
+import pymysql
+
+from lxml import etree
+from selenium import webdriver
+
+
+
+
+def get_first_page(url):
+
+    driver.get(url)
+    html = driver.page_source
+    return html
+
+
+
+# 可以尝试第二种解析方式，更加容易做计算
+def parse_stock_note(html):
+
+    selector = etree.HTML(html)
+    code = selector.xpath('//*[@id="pro_body"]/center/div[5]/h1/strong/text()')
+    profits= selector.xpath('//*[@id="right_col"]/table/tbody/tr[1]/td/table/tbody/tr[7]/td/text()')
+    d_2018= "".join(profits[1][:-3].split(","))
+    d_2017= "".join(profits[2][:-3].split(","))
+    d_2016= "".join(profits[3][:-3].split(","))
+
+    big_tuple = (code[0],d_2018,d_2017,d_2016)
+    return big_tuple
+
+
+
+
+
+
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='JS',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    cursor = connection.cursor()
+    try:
+        cursor.executemany('insert into js_FinData (name,d2018,d2017,d2016,industry) values (%s,%s,%s,%s,%s)', content)
+        connection.commit()
+        connection.close()
+        print('向MySQL中添加数据成功！')
+    except TypeError :
+        pass
+
+#
+if __name__ == '__main__':
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
+    url = 'https://www.japancupid.com/en/results/matches?pageno=3'
+    html = get_first_page(url)
+    content = parse_html(html)
+    print(content)
+
+
+
+
+# 因为板块数据是最后嵌套进去的，所以要保持，１．数据库表结构，２．解析整理后的数据结构　３．　插入的字段结构　三者之间都要保持一致
+# create table js_FinData(
+# id int not null primary key auto_increment,
+# name varchar(50),
+# d2018 varchar(20),
+# d2017 varchar(20),
+# d2016 varchar(20),
+# industry varchar(8)
+# ) engine=InnoDB  charset=utf8;
+
+#  drop table js_FinData;
